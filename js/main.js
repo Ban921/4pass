15477cc8bc5ea6b5180f8182870ca815a90ca489
jim <jim515jim@gmail.com>
2018-01-26 13:54:01 +0800

    modified:   js/main.js 	modified:   js/websocket.js

--- js/main.js
+++ js/main.js
@@ -5,324 +5,322 @@ var usernum = 0;
 var tablemoney = 0;
 
 function changnum(w, x, y, z) {
-	a = 0;
-	b = 0;
+    a = 0;
+    b = 0;
 
-	w = w % 13;
-	x = x % 13;
-	y = y % 13;
-	z = z % 13;
-	if (w == 0) {
-		w = 13;
-	}
-	if (x == 0) {
-		x = 13;
-	}
-	if (y == 0) {
-		y = 13;
-	}
-	if (z == 0) {
-		z = 13;
-	}
+    w = w % 13;
+    x = x % 13;
+    y = y % 13;
+    z = z % 13;
+    if (w == 0) {
+        w = 13;
+    }
+    if (x == 0) {
+        x = 13;
+    }
+    if (y == 0) {
+        y = 13;
+    }
+    if (z == 0) {
+        z = 13;
+    }
 
 
-	if (chk4pass(w, x, y, z)) {
-		a = 99;
-		b = 98;
-	} else {
-		//13等於兩張
-		if (chkpass(w, x, y, z)) {
+    if (chk4pass(w, x, y, z)) {
+        a = 99;
+        b = 98;
+    } else {
+        //13等於兩張
+        if (chkpass(w, x, y, z)) {
 
-		} else {
-			//12等於兩張大於10
-			if (chklong(w, x, y, z)) {} else {
-				a = 0;
-				b = 0;
-				num1(w, x);
-				num2(y, z);
-			}
-		}
-	}
+        } else {
+            //12等於兩張大於10
+            if (chklong(w, x, y, z)) {} else {
+                a = 0;
+                b = 0;
+                num1(w, x);
+                num2(y, z);
+            }
+        }
+    }
 
-	if (a > b) {
-		return false;
-	} else {
-		return true;
-	}
+    if (a > b) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 //轉點數
 function num1(w, x) {
-	if (w == 10) {
-		w = 0;
-	}
-	if (x == 10) {
-		x = 0;
-	}
-	//牌等於娃娃的時候 點數為0.5
-	if (w > 10) {
-		w = 0.5;
-	}
-	if (x > 10) {
-		x = 0.5;
-	}
+    if (w == 10) {
+        w = 0;
+    }
+    if (x == 10) {
+        x = 0;
+    }
+    //牌等於娃娃的時候 點數為0.5
+    if (w > 10) {
+        w = 0.5;
+    }
+    if (x > 10) {
+        x = 0.5;
+    }
 
-	if (a == '') {
-		a = w + x;
-	}
+    if (a == '') {
+        a = w + x;
+    }
 
-	if (a >= 10) {
-		a = a - 10;
-	}
+    if (a >= 10) {
+        a = a - 10;
+    }
 
-	return a;
+    return a;
 }
 
 function num2(y, z) {
-	if (y == 10) {
-		y = 0;
-	}
-	if (z == 10) {
-		z = 0;
-	}
-	//牌等於娃娃的時候 點數為0.5
-	if (y > 10) {
-		y = 0.5;
-	}
-	if (z > 10) {
-		z = 0.5;
-	}
+    if (y == 10) {
+        y = 0;
+    }
+    if (z == 10) {
+        z = 0;
+    }
+    //牌等於娃娃的時候 點數為0.5
+    if (y > 10) {
+        y = 0.5;
+    }
+    if (z > 10) {
+        z = 0.5;
+    }
 
-	if (b == '') {
-		b = y + z;
-	}
+    if (b == '') {
+        b = y + z;
+    }
 
-	if (b >= 10) {
-		b = b - 10;
-	}
+    if (b >= 10) {
+        b = b - 10;
+    }
 
-	return b;
+    return b;
 }
 
 //四支刀
 function chk4pass(w, x, y, z) {
-	if (w == x && x == y && y == z) {
-		return true;
-	}
-	return false;
+    if (w == x && x == y && y == z) {
+        return true;
+    }
+    return false;
 }
 //兩張一樣
 function chkpass(w, x, y, z) {
-	if (w == x) {
-		if (w == 1) {
-			a = w + 12 + 13;
-		} else {
-			a = w + 12;
-		}
+    if (w == x) {
+        if (w == 1) {
+            a = w + 12 + 13;
+        } else {
+            a = w + 12;
+        }
 
-	} else {
-		if (chklong(w, x, y, z)) {} else {
-			a = num1(w, x);
-		}
-	}
-	if (y == z) {
-		if (y == 1) {
-			b = y + 12 + 13;
-		} else {
-			b = y + 12;
-		}
-	} else {
-		if (chklong(w, x, y, z)) {} else {
-			b = num2(y, z);
-		}
-	}
-	if (a == w + 12 || b == y + 12 || a == w + 12 + 13 || b == y + 12 + 13) {
-		return true;
-	}
-	return false;
+    } else {
+        if (chklong(w, x, y, z)) {} else {
+            a = num1(w, x);
+        }
+    }
+    if (y == z) {
+        if (y == 1) {
+            b = y + 12 + 13;
+        } else {
+            b = y + 12;
+        }
+    } else {
+        if (chklong(w, x, y, z)) {} else {
+            b = num2(y, z);
+        }
+    }
+    if (a == w + 12 || b == y + 12 || a == w + 12 + 13 || b == y + 12 + 13) {
+        return true;
+    }
+    return false;
 }
 //囉嗦
 function chklong(w, x, y, z) {
-	if (w > 10 && x > 10) {
-		a = 12;
-	} else {
-		if (a == '') {
-			a = num1(w, x);
-		}
-	}
-	if (y > 10 && z > 10) {
-		b = 12;
-	} else {
-		if (b == '') {
-			b = num2(y, z);
-		}
-	}
-	if (a == 12 || b == 12) {
-		return true;
-	}
-	return false;
+    if (w > 10 && x > 10) {
+        a = 12;
+    } else {
+        if (a == '') {
+            a = num1(w, x);
+        }
+    }
+    if (y > 10 && z > 10) {
+        b = 12;
+    } else {
+        if (b == '') {
+            b = num2(y, z);
+        }
+    }
+    if (a == 12 || b == 12) {
+        return true;
+    }
+    return false;
 }
 
 
 
-$('.start').click(function () {
-	websocket.send(JSON.stringify({
-		'action': "start",
-		'name': name,
-	}));
+$('.start').click(function() {
+    websocket.send(JSON.stringify({
+        'action': "start",
+        'name': name,
+    }));
 });
 
-$('.gitporker').click(function () {
-	raisenum = 0;
-	usernum = 0;
-	for (var i = 1; i < 9; i++) {
-		if (user[i]) {
-			usernum++
-		};
-	}
-	if (user[id]) {
-		gamemoney = parseInt($(".money").html());
-		$(".money").html(gamemoney - 10);
-		$('.raise , .flod, .tablemoney, .yes').show();
-	}
-	$('.start').hide();
-	$('.user' + id).sortable({
-		update: function (event, ui) {
-			w = parseInt($('.user' + id).find("img").eq(0).attr('data'));
-			x = parseInt($('.user' + id).find("img").eq(1).attr('data'));
-			y = parseInt($('.user' + id).find("img").eq(2).attr('data'));
-			z = parseInt($('.user' + id).find("img").eq(3).attr('data'));
-			changnum(w, x, y, z);
-			log('第一副點數' + a + '第二副點數' + b);
+$('.gitporker').click(function() {
+    raisenum = 0;
+    usernum = 0;
+    for (var i = 1; i < 9; i++) {
+        if (user[i]) {
+            usernum++
+        };
+    }
+    if (user[id]) {
+        gamemoney = parseInt($(".money").html());
+        $(".money").html(gamemoney - 10);
+        $('.raise , .flod, .tablemoney, .yes').show();
+    }
+    $('.start').hide();
+    $('.user' + id).sortable({
+        update: function(event, ui) {
+            w = parseInt($('.user' + id).find("img").eq(0).attr('data'));
+            x = parseInt($('.user' + id).find("img").eq(1).attr('data'));
+            y = parseInt($('.user' + id).find("img").eq(2).attr('data'));
+            z = parseInt($('.user' + id).find("img").eq(3).attr('data'));
+            changnum(w, x, y, z);
+            log('第一副點數' + a + '第二副點數' + b);
 
-		}
-	});
-
-});
-$('.yes').click(function () {
-	if (raisemin == raisemax) {
-		w = parseInt($('.user' + id).find("img").eq(0).attr('data'));
-		x = parseInt($('.user' + id).find("img").eq(1).attr('data'));
-		y = parseInt($('.user' + id).find("img").eq(2).attr('data'));
-		z = parseInt($('.user' + id).find("img").eq(3).attr('data'));
-
-		if (changnum(w, x, y, z)) {
-			$('.raise,.call,.flod,.check,.yes').hide();
-
-			websocket.send(JSON.stringify({
-				'action': "yes",
-				'name': name,
-				'userid': id,
-				'upnum': a,
-				'downunm': b,
-				'w': w,
-				'x': x,
-				'y': y,
-				'z': z,
-			}));
-		} else {
-			alert("前墩不能大於後墩");
-		}
-	} else {
-		alert("請等大家下注完畢");
-	}
-
+        }
+    });
 
 });
+$('.yes').click(function() {
+    if (raisenum1 == raisemax) {
+        w = parseInt($('.user' + id).find("img").eq(0).attr('data'));
+        x = parseInt($('.user' + id).find("img").eq(1).attr('data'));
+        y = parseInt($('.user' + id).find("img").eq(2).attr('data'));
+        z = parseInt($('.user' + id).find("img").eq(3).attr('data'));
 
-$('.check').click(function () {
-	var usernum = 0;
-	for (var i = 1; i < 9; i++) {
-		if (user[i]) {
-			if (i != id) {
-				usernum++
-			}
-		}
-	}
+        if (changnum(w, x, y, z)) {
+            $('.raise,.call,.flod,.check,.yes').hide();
 
-	tablemoney = parseInt($(".tablemoney").html());
-	$('.tablemoney').html(tablemoney);
+            websocket.send(JSON.stringify({
+                'action': "yes",
+                'name': name,
+                'userid': id,
+                'upnum': a,
+                'downunm': b,
+                'w': w,
+                'x': x,
+                'y': y,
+                'z': z,
+            }));
+        } else {
+            alert("前墩不能大於後墩");
+        }
+    } else {
+        alert("請等大家下注完畢");
+    }
+});
+
+$('.check').click(function() {
+    var usernum = 0;
+    for (var i = 1; i < 9; i++) {
+        if (user[i]) {
+            if (i != id) {
+                usernum++
+            }
+        }
+    }
+
+    tablemoney = parseInt($(".tablemoney").html());
+    $('.tablemoney').html(tablemoney);
 
 });
 
-$('.flod').click(function () {
-	websocket.send(JSON.stringify({
-		'action': "flod",
-		'name': name,
-		'userid': id,
-	}));
-	tablemoney = 0;
-	$('.tablemoney').html(tablemoney);
-	$('.raise,.call,.flod,.check,.yes').hide();
+$('.flod').click(function() {
+    websocket.send(JSON.stringify({
+        'action': "flod",
+        'name': name,
+        'userid': id,
+    }));
+    tablemoney = 0;
+    $('.tablemoney').html(tablemoney);
+    $('.raise,.call,.flod,.check,.yes').hide();
 });
 
 
-$('.raise').click(function () {
-	gamemoney = parseInt($(".money").html());
-	if (gamemoney > 0) {
-		raisenum1++;
-		$(".money").html(gamemoney - 10);
-		tablemoney = tablemoney + 10;
-		$('.tablemoney').html(tablemoney);
-		websocket.send(JSON.stringify({
-			'action': "raise",
-			'name': name,
-			'userid': id,
-			'raisenum': raisenum1,
-			'num': 1,
-		}));
-	} else {
-		alert("錢不夠");
-		return false;
-	}
+$('.raise').click(function() {
+    gamemoney = parseInt($(".money").html());
+    if (gamemoney > 0) {
+        raisenum1++;
+        $(".money").html(gamemoney - 10);
+        tablemoney = tablemoney + 10;
+        $('.tablemoney').html(tablemoney);
+        websocket.send(JSON.stringify({
+            'action': "raise",
+            'name': name,
+            'userid': id,
+            'raisenum': raisenum1,
+            'num': 1,
+        }));
+    } else {
+        alert("錢不夠");
+        return false;
+    }
 
 });
-$('.call').click(function () {
-	gamemoney = parseInt($(".money").html());
-	if (gamemoney >= 10 * raisemax) {
-		raisenum = raisemax;
-		raisenum1 = raisemax;
-		$(".money").html(gamemoney - 10 * raisemax);
-		websocket.send(JSON.stringify({
-			'action': "raise",
-			'name': name,
-			'userid': id,
-			'raisenum': raisenum,
-			'num': raisemax,
-		}));
-	} else {
-		alert("錢不夠");
-		return false;
-	}
+$('.call').click(function() {
+    gamemoney = parseInt($(".money").html());
+    if (gamemoney >= 10 * raisenum1) {
+        raisenum1 = raisemax - raisenum1;
+        $(".money").html(gamemoney - 10 * raisenum1);
+        raisenum1 = raisemax;
+        websocket.send(JSON.stringify({
+            'action': "raise",
+            'name': name,
+            'userid': id,
+            'raisenum': raisenum1,
+            'num': raisenum1,
+        }));
+    } else {
+        alert("錢不夠");
+        return false;
+    }
 });
 
-$('.web').click(function () {
-	websocket.send(JSON.stringify({
-		'action': "ready",
-		'name': name,
-	}));
+$('.web').click(function() {
+    websocket.send(JSON.stringify({
+        'action': "ready",
+        'name': name,
+    }));
 });
 
-$('.restart').click(function () {
-	websocket.send(JSON.stringify({
-		'action': "restart",
-		'name': name,
-	}));
+$('.restart').click(function() {
+    websocket.send(JSON.stringify({
+        'action': "restart",
+        'name': name,
+    }));
 });
 
-$(function () {
-	$('#message').keypress(function (e) {
-		if (e.keyCode == 13 && this.value) {
-			websocket.send(JSON.stringify({
-				'action': "masg",
-				'name': name,
-				'masg': this.value,
-			}));
-			$(this).val('');
-		}
-	});
+$(function() {
+    $('#message').keypress(function(e) {
+        if (e.keyCode == 13 && this.value) {
+            websocket.send(JSON.stringify({
+                'action': "masg",
+                'name': name,
+                'masg': this.value,
+            }));
+            $(this).val('');
+        }
+    });
 });
 
-if (!name) {
-	var name = prompt("請輸入遊戲名稱");
+while (!name) {
+    var name = prompt("請輸入遊戲名稱");
 }
--- js/websocket.js
+++ js/websocket.js
@@ -6,179 +6,179 @@ var raisemax = '';
 var raisemin = '';
 var user = new Array();
 
-websocket.onopen = function (e) {
-	$('.web').click();
-	log("連線成功");
+websocket.onopen = function(e) {
+    $('.web').click();
+    log("連線成功");
 };
-websocket.onmessage = function (e) {
-	if (isJSON(e.data)) {
-		data = JSON.parse(e.data);
+websocket.onmessage = function(e) {
+    if (isJSON(e.data)) {
+        data = JSON.parse(e.data);
 
-		switch (data.action) {
-			case "index_id":
-				id = data.id;
+        switch (data.action) {
+            case "index_id":
+                id = data.id;
 
-				break;
-			case "ready":
-				log("收到服务端的消息：" + data.name + "已準備");
-				if (data.onlinegame[0] == id && data.gameing == 0) {
-					$('.start').show();
-				}
-				break;
-			case "masg":
-				log(data.name + "傳送了:" + data.masg);
-				break;
-			case "start":
-				raisenum = '';
-				raisemax = 0;
-				raisemin = 0;
-				raisenum1 = 0;
-				if (data.userid) {
-					user[data.userid] = false;
-					$('.user' + data.userid).empty();
-					$('.user' + data.userid).css("border-style", "");
-				}
+                break;
+            case "ready":
+                log("收到服务端的消息：" + data.name + "已準備");
+                if (data.onlinegame[0] == id && data.gameing == 0) {
+                    $('.start').show();
+                }
+                break;
+            case "masg":
+                log(data.name + "傳送了:" + data.masg);
+                break;
+            case "start":
+                raisenum = '';
+                raisemax = 0;
+                raisemin = 0;
+                raisenum1 = 0;
 
-				for (var i = 0; i < data.online.length; i++) {
-					user[data.online[i]] = true;
-				}
-				$('.gitporker').click();
+                for (var i = 0; i <= data.online.length; i++) {
+                    user[data.online[i]] = true;
+                }
+                $('.gitporker').click();
 
-				poker = data.poker;
-				for (var i = 1; i < 9; i++) {
-					if (user[i]) {
-						$('.user' + i).css("border-style", "dashed");
-						$('.user' + i).css("border-color", "black");
-						$('.user' + i).html('<img src="img/Red_Back.svg" />' + '<img src="img/Red_Back.svg" />' + '<img src="img/Red_Back.svg" />' + '<img src="img/Red_Back.svg" />');
+                poker = data.poker;
+                for (var i = 1; i < 9; i++) {
+                    if (user[i]) {
+                        $('.user' + i).css("border-style", "dashed");
+                        $('.user' + i).css("border-color", "black");
+                        $('.user' + i).html('<img src="img/Red_Back.svg" />' + '<img src="img/Red_Back.svg" />' + '<img src="img/Red_Back.svg" />' + '<img src="img/Red_Back.svg" />');
 
-						if (i == id) {
-							$('.user' + id).html('<img data="' + poker[0 + (i * 4)] + '" src="img/' + poker[0 + (i * 4)] + '.svg" />' + '<img data="' + poker[1 + (i * 4)] + '" src="img/' + poker[1 + (i * 4)] + '.svg" />' + '<img data="' + poker[2 + (i * 4)] + '" src="img/' + poker[2 + (i * 4)] + '.svg" />' + '<img data="' + poker[3 + (i * 4)] + '" src="img/' + poker[3 + (i * 4)] + '.svg" />');
-							$('.porkeropen').html('<img data="' + poker[0 + (i * 4)] + '" src="img/' + poker[0 + (i * 4)] + '.svg" />' + '<img data="' + poker[1 + (i * 4)] + '" src="img/' + poker[1 + (i * 4)] + '.svg" />' + '<img data="' + poker[2 + (i * 4)] + '" src="img/' + poker[2 + (i * 4)] + '.svg" />' + '<img data="' + poker[3 + (i * 4)] + '" src="img/' + poker[3 + (i * 4)] + '.svg" />');
-						}
-					} else {
-						$('.user' + i).empty();
-						$('.user' + i).css("border-style", "");
-					}
-				}
-				$('.online').html('');
+                        if (i == id) {
+                            $('.user' + id).html('<img data="' + poker[0 + (i * 4)] + '" src="img/' + poker[0 + (i * 4)] + '.svg" />' + '<img data="' + poker[1 + (i * 4)] + '" src="img/' + poker[1 + (i * 4)] + '.svg" />' + '<img data="' + poker[2 + (i * 4)] + '" src="img/' + poker[2 + (i * 4)] + '.svg" />' + '<img data="' + poker[3 + (i * 4)] + '" src="img/' + poker[3 + (i * 4)] + '.svg" />');
+                            $('.porkeropen').html('<img data="' + poker[0 + (i * 4)] + '" src="img/' + poker[0 + (i * 4)] + '.svg" />' + '<img data="' + poker[1 + (i * 4)] + '" src="img/' + poker[1 + (i * 4)] + '.svg" />' + '<img data="' + poker[2 + (i * 4)] + '" src="img/' + poker[2 + (i * 4)] + '.svg" />' + '<img data="' + poker[3 + (i * 4)] + '" src="img/' + poker[3 + (i * 4)] + '.svg" />');
+                        }
+                    } else {
+                        $('.user' + i).empty();
+                        $('.user' + i).css("border-style", "");
+                    }
+                }
+                $('.online').html('');
 
-				break;
-			case "check":
-				for (var i = 0; i < data.online.length; i++) {
-					user[data.online[i]] = true;
-				}
-				$('.check').click();
-				var upnum = [];
-				var downunm = [];
-				pokered = data.pokered;
-				for (var i = 1; i < 9; i++) {
-					if (user[i]) {
-						$('.user' + i).html('<img data="' + pokered[0 + (i * 4)] + '" src="img/' + pokered[0 + (i * 4)] + '.svg" />' + '<img data="' + pokered[1 + (i * 4)] + '" src="img/' + pokered[1 + (i * 4)] + '.svg" />' + '<img data="' + pokered[2 + (i * 4)] + '" src="img/' + pokered[2 + (i * 4)] + '.svg" />' + '<img data="' + pokered[3 + (i * 4)] + '" src="img/' + pokered[3 + (i * 4)] + '.svg" />');
-						changnum(pokered[0 + (i * 4)], pokered[1 + (i * 4)], pokered[2 + (i * 4)], pokered[3 + (i * 4)]);
-						$('.online').append(data.name1[i] + "第一副牌" + a + "第二副牌" + b + "<br>");
-					}
-				}
-				$('.user' + data.id).css("border-color", "red");
+                break;
+            case "check":
+                for (var i = 0; i <= data.online.length; i++) {
+                    user[data.online[i]] = true;
+                }
+                $('.check').click();
+                var upnum = [];
+                var downunm = [];
+                pokered = data.pokered;
+                for (var i = 0; i <= data.online.length; i++) {
+                    if (user[i]) {
+                        console.log(i, data.online);
+                        $('.user' + i).html('<img src="img/' + pokered[0 + (i * 4)] + '.svg" />' + '<img src="img/' + pokered[1 + (i * 4)] + '.svg" />' + '<img src="img/' + pokered[2 + (i * 4)] + '.svg" />' + '<img src="img/' + pokered[3 + (i * 4)] + '.svg" />');
+                        changnum(pokered[0 + (i * 4)], pokered[1 + (i * 4)], pokered[2 + (i * 4)], pokered[3 + (i * 4)]);
+                        $('.online').append(data.name1[i] + "第一副牌" + a + "第二副牌" + b + "<br>");
+                    }
+                }
+                $('.user' + data.id).css("border-color", "red");
+                $('.call').hide();
+                if (data.id == id) {
+                    money = parseInt($(".money").html());
+                    $('.money').html(data.money + money);
+                    log('賺了' + data.money);
+                }
+                if (data.lost) {
+                    for (var i = 0; i <= data.lost.length; i++) {
+                        user[data.lost[i]] = false;
+                        $('.user' + data.lost[i]).empty();
+                        $('.user' + data.lost[i]).css("border-style", "");
+                        log(data.name1[data.lost[i]] + "被夾了");
+                    }
+                }
+                if (data.online[0] == id) {
+                    setTimeout(function() {
+                        $('.tablemoney').html('');
+                        $('.start').show();
+                    }, 2000);
+                } else {
+                    setTimeout(function() {
+                        $('.tablemoney').html('');
+                    }, 2000);
+                }
+                break;
+            case "raise":
+                $('.tablemoney').html(data.tablemoney);
+                raisemax = data.raisemax;
+                raisemin = data.raisemin;
 
-				if (data.id == id) {
-					money = parseInt($(".money").html());
-					$('.money').html(data.money + money);
-					log('賺了' + data.money);
-				}
-				if (data.lost) {
-					for (var i = 0; i < data.lost.length; i++) {
-						user[data.lost[i]] = false;
-						$('.user' + data.lost[i]).empty();
-						$('.user' + data.lost[i]).css("border-style", "");
-						log(data.name1[data.lost[i]] + "被夾了");
-					}
-				}
-				if (data.online[0] == id) {
-					setTimeout(function () {
-						$('.tablemoney').html('');
-						$('.start').show();
-					}, 2000);
-				} else {
-					setTimeout(function () {
-						$('.tablemoney').html('');
-					}, 2000);
-				}
-				break;
-			case "raise":
-				$('.tablemoney').html(data.tablemoney);
-				raisemax = data.raisemax;
-				raisemin = data.raisemin;
+                raisenum = '';
 
-				raisenum = '';
+                for (i in data.raisenum) {
+                    raisenum = raisenum + data.name1[i] + "加碼次數" + data.raisenum[i] + "<br>";
+                }
+                $('.online').html(raisenum);
+                if (raisenum1 < raisemax) {
+                    $('.call').show();
+                    $('.flod').show();
 
-				for (i in data.raisenum) {
-					raisenum = raisenum + data.name1[i] + "加碼次數" + data.raisenum[i] + "<br>";
-				}
-				$('.online').html(raisenum);
-				if (raisenum1 < raisemax) {
-					$('.call').show();
-				} else {
-					$('.call').hide();
-				}
+                } else {
+                    $('.call').hide();
+                }
 
-				break;
-			case "flod":
-				user[data.userid] = false;
-				$('.user' + data.userid).hide();
-				$('.online').html();
+                break;
+            case "flod":
+                user[data.userid] = false;
+                $('.user' + data.userid).empty();
+                $('.user' + data.userid).css("border-style", "");
+                $('.online').html();
+                raisemax = data.raisemax;
+                raisemin = data.raisemin;
+                break;
+            case "quit":
+                user[data.userid] = false;
+                $('.user' + data.userid).empty();
+                $('.user' + data.userid).css("border-style", "");
+                break;
+        }
 
-				break;
-			case "quit":
-				user[data.userid] = false;
-				$('.user' + data.userid).empty();
-				$('.user' + data.userid).css("border-style", "");
-				break;
-		}
+        if (data.msg) {
+            log(data.msg);
+        }
+        if (data.unid) {
+            user[data.unid] = false;
+        }
+        tablemoney = data.tablemoney;
+        $('.tablemoney').html(tablemoney);
 
-		if (data.msg) {
-			log(data.msg);
-		}
-		if (data.unid) {
-			user[data.unid] = false;
-		}
-		tablemoney = data.tablemoney;
-		$('.tablemoney').html(tablemoney);
-
-	} else {
-		if (e.data) {
-			log(e.data);
-		}
-	}
+    } else {
+        if (e.data) {
+            log(e.data);
+        }
+    }
 };
-websocket.onerror = function (e) {
-	console.log(e);
-	websocket.close();
+websocket.onerror = function(e) {
+    console.log(e);
+    websocket.close();
 };
-websocket.onclose = function (e) {
-	log("斷線 5 秒後重新整理");
+websocket.onclose = function(e) {
+    log("斷線 5 秒後重新整理");
 
-	websocket.close();
+    websocket.close();
 
-	setTimeout(function () {
-		location.reload();
-	}, 5000);
+    setTimeout(function() {
+        location.reload();
+    }, 5000);
 };
 
 function log(text) {
-	$log = $('#log');
-	//Add text to log
-	$log.append(($log.val() ? "\n" : '') + text);
-	//Autoscroll
-	$log[0].scrollTop = $log[0].scrollHeight - $log[0].clientHeight;
+    $log = $('#log');
+    //Add text to log
+    $log.append(($log.val() ? "\n" : '') + text);
+    //Autoscroll
+    $log[0].scrollTop = $log[0].scrollHeight - $log[0].clientHeight;
 }
 
 function isJSON(str) {
-	if (typeof str == 'string') {
-		try {
-			JSON.parse(str);
-			return true;
-		} catch (e) {
-			return false;
-		}
-	}
+    if (typeof str == 'string') {
+        try {
+            JSON.parse(str);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
 }
